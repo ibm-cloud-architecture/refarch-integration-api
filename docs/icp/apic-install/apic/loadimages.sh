254acde9f89b6f688a6359f6ca2bd64c3d38b439
rengan <rsundara@us.ibm.com>
2018-11-30 20:21:19 -0500

    Add namespace parameter when loading the images

--- docs/icp/apic-install/apic/loadimages.sh
+++ docs/icp/apic-install/apic/loadimages.sh
@@ -9,7 +9,7 @@ IMAGE_DIR=/DIRECTORY_HAVING_IMAGES
 
 # Load PPA Archives
 cd $IMAGE_DIR
-cloudctl catalog load-ppa-archive --archive $IMAGE_DIR/analytics-images-icp.tgz --registry $CLUSTER_NAME:8500
-cloudctl catalog load-ppa-archive --archive $IMAGE_DIR/gateway-images-icp.tgz --registry $CLUSTER_NAME:8500
-cloudctl catalog load-ppa-archive --archive $IMAGE_DIR/management-images-icp.tgz --registry $CLUSTER_NAME:8500
-cloudctl catalog load-ppa-archive --archive $IMAGE_DIR/portal-images-icp.tgz --registry $CLUSTER_NAME:8500
+cloudctl catalog load-ppa-archive --archive $IMAGE_DIR/analytics-images-icp.tgz --registry $CLUSTER_NAME:8500/apiconnect
+cloudctl catalog load-ppa-archive --archive $IMAGE_DIR/gateway-images-icp.tgz --registry $CLUSTER_NAME:8500/apiconnect
+cloudctl catalog load-ppa-archive --archive $IMAGE_DIR/management-images-icp.tgz --registry $CLUSTER_NAME:8500/apiconnect
+cloudctl catalog load-ppa-archive --archive $IMAGE_DIR/portal-images-icp.tgz --registry $CLUSTER_NAME:8500/apiconnect
